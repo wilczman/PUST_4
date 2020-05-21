db07750ab38e8233dbdc89a3f3c244aef8824733
wilczman <38138814+wilczman@users.noreply.github.com>
2020-05-21 16:59:02 +0200

    zad2 (done)

---
+++ zad2_odp_skok.m
@@ -0,0 +1,88 @@
+clear all;
+%Dlugosc symulacji
+kk=300;
+%Ograniczenia
+u_min=1;
+u_max=2;
+%Punkt Pracy
+U_pp=0;
+Y_pp=0;
+
+U{1}=zeros(1, kk);
+U{2}=zeros(1, kk);
+U{3}=zeros(1, kk);
+U{4}=zeros(1, kk);
+U{1}(1:kk)=U_pp;
+U{2}(1:kk)=U_pp;
+U{3}(1:kk)=U_pp;
+U{4}(1:kk)=U_pp;
+Y{1}=zeros(1, kk);
+Y{2}=zeros(1, kk);
+Y{3}=zeros(1, kk);
+Y{1}(1:11)=Y_pp;
+Y{2}(1:11)=Y_pp;
+Y{3}(1:11)=Y_pp;
+
+for i=1:4
+    U{i}=zeros(1, kk)+1;
+    for k=1:kk
+       if k>4
+            [Y{1}(k),Y{2}(k),Y{3}(k)] = symulacja_obiektu5( ...
+            U{1}(k-1),U{1}(k-2),U{1}(k-3),U{1}(k-4),...
+            U{2}(k-1),U{2}(k-2),U{2}(k-3),U{2}(k-4),...
+            U{3}(k-1),U{3}(k-2),U{3}(k-3),U{3}(k-4),...
+            U{4}(k-1),U{4}(k-2),U{4}(k-3),U{4}(k-4),...
+            Y{1}(k-1),Y{1}(k-2),Y{1}(k-3),Y{1}(k-4),...
+            Y{2}(k-1),Y{2}(k-2),Y{2}(k-3),Y{2}(k-4),...
+            Y{3}(k-1),Y{3}(k-2),Y{3}(k-3),Y{3}(k-4));  
+       end
+    end
+    odp_skok{i}{1} = Y{1}; Y{1} = zeros(1, kk);
+    odp_skok{i}{2} = Y{2}; Y{2} = zeros(1, kk);
+    odp_skok{i}{3} = Y{3}; Y{3} = zeros(1, kk);
+    U{i} = zeros(1, kk);
+end
+
+%WYKRESY
+    figure 
+    hold on;
+    for i=1:4
+        subplot(4,3,i*3-2); stairs(odp_skok{i}{1});title(sprintf('Skok sygna³u steruj¹cego u%d(k)',i));xlabel('k');ylabel('wartoœæ sygna³u y1(k)');
+        subplot(4,3,i*3-1); stairs(odp_skok{i}{2});title(sprintf('Skok sygna³u steruj¹cego u%d(k)',i));xlabel('k');ylabel('wartoœæ sygna³u y2(k)');
+        subplot(4,3,i*3); stairs(odp_skok{i}{3});title(sprintf('Skok sygna³u steruj¹cego u%d(k)',i));xlabel('k');ylabel('wartoœæ sygna³u y3(k)');
+    end
+ 
+
+    
+    
+
+%%%ZAPIS DO PLIKU%%%%%%%
+% j=0; k=0;
+% for k=1:kk       
+%        j=j+1;
+%        table_Y(1,k)=double(j);
+%        table_U(1,k)=double(j);
+% end
+% %macierze wykorzystane do zapisu
+% table_Y(2,:)=Y;
+% table_U(2,:)=U;
+% 
+% fname_Y = sprintf('ppY.txt');
+% fname_U = sprintf('ppU.txt');
+% mkdir_status_Y=mkdir(sprintf('C:\\Users\\Kuba\\Desktop\\GIT\\PUST_3\\wykres_punkt_pracy'));
+% if mkdir_status_Y
+%    savdir_Y = sprintf('C:\\Users\\Kuba\\Desktop\\GIT\\PUST_3\\wykres_punkt_pracy\\');
+% 
+%    fileID = fopen([savdir_Y fname_Y],'w');
+%    fprintf(fileID,'%6.3f %6.3f\r\n',table_Y);
+%    fclose(fileID);
+%    fileID = fopen([savdir_Y fname_U],'w');
+%    fprintf(fileID,'%6.3f %6.3f\r\n',table_U);
+%    fclose(fileID);
+%    
+% else 
+%    disp('Nie udalo sie stworzyc folderów')
+% end
+% warning('on','all') %wlaczenie warningow
+
+   
