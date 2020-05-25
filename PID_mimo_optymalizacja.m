4562fa57a879a961f9548a68d52b6747eea6b988
wilczman <38138814+wilczman@users.noreply.github.com>
2020-05-25 15:36:20 +0200

    nastrojony PID, zapisane odpowiedzi skokowe do DMC

--- PID_mimo_optymalizacja.m
+++ PID_mimo_optymalizacja.m
@@ -1,7 +1,7 @@
-x0 = [2, 22, 5];
-lb = [0.001 0.001 0.001];
-
-[nastawy_PID_fmincon,E]=fmincon((@(parameters) PID_mimo_fun(parameters)),x0,[],[],[],[],lb,[]);
+x0 = [1, 1, 1];
+lb = [0.0001 0.0001 0.0001];
+nr=4;
+[nastawy_PID_fmincon,E]=fmincon((@(parameters) PID_mimo_fun(parameters,nr)),x0,[],[],[],[],lb,[]);
 %[nastawy_PID_ga, ~, ~] = ga((@(parameters) PID_fun(parameters)), 3, [], [], [], [], [0 0.1 0], []);
 save('optymalne_parametry_PID_zad3.mat', 'nastawy_PID_fmincon');
 
